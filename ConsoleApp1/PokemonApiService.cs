6a9507f752f407f8e84a0c5eda1c22f3ebc398cf
Leonardo Nunes <leonardonmattoslopes@gmail.com>
2023-09-15 20:22:20 -0300

    Implementando o menu interativo com o usuario

---
+++ ConsoleApp1/PokemonApiService.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class PokemonApiService
+    {
+        public List<PokemonResult> ObterEspeciesDisponiveis()
+        {
+            // Obter a lista de espécies de pokémons 
+            var client = new RestClient("https://pokeapi.co/api/v2/pokemon-species/");
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+
+            var pokemonEspeciesResposta = JsonConvert.DeserializeObject<PokemonSpeciesResult>(response.Content);
+
+            return pokemonEspeciesResposta.Results;
+        }
+
+        public PokemonDetailsResults ObterDetalhesDaEspecie(PokemonResult especie)
+        {
+            // Obter as características do Pokémon escolhido
+            var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{especie.Name}");
+            var request = new RestRequest(Method.GET);
+            var response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<PokemonDetailsResults>(response.Content);
+
+        }
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,68 +1,75 @@
 ﻿using ConsoleApp1;
 using Newtonsoft.Json;
 using RestSharp;
-using System;
 
 public class Program
 {
     public static void Main (string[] args)
     {
-        //Obter a lista de espécies de pokémons
-
-        var client = new RestClient("https://pokeapi.co/api/v2/pokemon-species/");
-        var request = new RestRequest(Method.GET);
-        IRestResponse response = client.Execute(request);
-
-        var pokemonEspeciesResposta = JsonConvert.DeserializeObject<PokemonSpeciesResult>(response.Content);
-
-        //Apresentar as opções ao jogador
-        Console.WriteLine("Escolha um Tamagotchi: ");
-        for (int i = 0; i < pokemonEspeciesResposta.Results.Count; i++)
-        {
-            Console.WriteLine(i + 1 + "." + pokemonEspeciesResposta.Results[i].Name);
-        }
-
-        //Obter a escolha do jogador
-        int escolha;
-
-
+        Menu menu = new Menu();
+        PokemonApiService pokemonApiService = new PokemonApiService();
+        List<PokemonResult> especiesDisponiveis = pokemonApiService.ObterEspeciesDisponiveis();
+        List<PokemonDetailsResults> mascotesAdotados = new List<PokemonDetailsResults>();
+        menu.MostrarMensagemDeBoasVindas();
 
         while (true)
         {
-            Console.WriteLine("\n");
-            Console.WriteLine("Escolha um número: ");
-            if (!int.TryParse(Console.ReadLine(), out escolha) && escolha >= 1 && escolha <= pokemonEspeciesResposta.Results.Count)
+            menu.MostrarMenuPrincipal();
+            int escolha = menu.ObterEscolhaDoJogador();
+
+            switch (escolha)
             {
-                Console.WriteLine("Escolha inválida. Tente novamente.");
+                case 1:
+                    while (true)
+                    {
+                        menu.MostrarMenuDeAdocao();
+                        escolha = menu.ObterEscolhaDoJogador();
+                        switch (escolha)
+                        {
+                            case 1:
+                                menu.MostrarEspeciesDisponiveis(especiesDisponiveis);
+                                break;
+                            case 2:
+                                menu.MostrarEspeciesDisponiveis(especiesDisponiveis);
+                                int indiceEspecie = menu.ObterEspecieEscolhida(especiesDisponiveis);
+                                PokemonDetailsResults detalhes = pokemonApiService.ObterDetalhesDaEspecie(especiesDisponiveis[indiceEspecie]);
+                                menu.MostrarDetalhesDaEspecie(detalhes);
+                                break;
+                            case 3:
+                                menu.MostrarEspeciesDisponiveis(especiesDisponiveis);
+                                indiceEspecie = menu.ObterEspecieEscolhida(especiesDisponiveis);
+                                detalhes = pokemonApiService.ObterDetalhesDaEspecie(especiesDisponiveis[indiceEspecie]);
+                                menu.MostrarDetalhesDaEspecie(detalhes);
+                                if (menu.ConfirmarAdocao())
+                                {
+                                    mascotesAdotados.Add(detalhes);
+                                    Console.WriteLine("Parabéns! Você adotou um " + detalhes.Name + "!");
+                                    Console.WriteLine("──────────────");
+                                    Console.WriteLine("────▄████▄────");
+                                    Console.WriteLine("──▄████████▄──");
+                                    Console.WriteLine("──██████████──");
+                                    Console.WriteLine("──▀████████▀──");
+                                    Console.WriteLine("─────▀██▀─────");
+                                    Console.WriteLine("──────────────");
+                                }
+                                break;
+                            case 4:
+                                break;
+                        }
+                        if (escolha == 4)
+                        {
+                            break;
+                        }
+                    }
+                    break;
+                case 2:
+                    menu.MostrarMascotesAdotados(mascotesAdotados);
+                    break;
+                case 3:
+                    Console.WriteLine("Obrigado por jogar! Até a próxima!");
+                    return;
             }
-            else
-                break;
         }
-
-        // Obter as características do Pokémon escolhido
-        client = new RestClient($"https://pokeapi.co/api/v2/pokemon/" + escolha);
-        request = new RestRequest(Method.GET);
-        response = client.Execute(request);
-
-        var pokemonDetalhes = JsonConvert.DeserializeObject<PokemonDetailsResults>(response.Content);
-
-        var pokemonEscolhido = pokemonEspeciesResposta.Results[escolha - 1];
-
-        //Mostrar as características ao jogador
-        Console.WriteLine("\n");
-        Console.WriteLine("Você escolheu " + pokemonEscolhido.Name + "!");
-        Console.WriteLine("Detahles: ");
-        Console.WriteLine("- Nome: " + pokemonEscolhido.Name);
-        Console.WriteLine("- Peso: " + pokemonDetalhes.weight);
-        Console.WriteLine("- Altura: " + pokemonDetalhes.Height);
-
-        Console.WriteLine("\n Habilidades do Mascote: ");
-
-        foreach(var abiliyDetail in pokemonDetalhes.Abilities)
-        {
-            Console.WriteLine("Nome da habilidade: " + abiliyDetail.Ability.Name);
-        }
-
-        Console.WriteLine("\n");
     }
+
 }
